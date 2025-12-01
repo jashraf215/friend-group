273a54bf2b40ad22a379f075da69bec285683b35
Jibril <ucapja2@ucl.ac.uk>
2025-12-01 02:03:06 +0000

    Completed initial_two_classes implementation

--- refactoring/initial_two_classes.py
+++ refactoring/initial_two_classes.py
@@ -18,7 +18,7 @@ class Group:
 
     def size(self):
         """Return how many people are in the group."""
-        pass
+        return len(self.members)
 
     def contains(self, name):
         """Check whether the group contains a person with the given name.
@@ -29,20 +29,35 @@ class Group:
     def add_person(self, name, age, job):
         """Add a new person with the given characteristics to the group."""
         self.members.append(Person(name, age, job))
+        if name not in self.connections:
+            self.connections[name] = {}
 
     def number_of_connections(self, name):
         """Find the number of connections that a person in the group has"""
-        pass
+        if name in self.connections:
+            return len(self.connections[name])
+        return 0
 
     def connect(self, name1, name2, relation, reciprocal=True):
         """Connect two given people in a particular way.
         Optional reciprocal: If true, will add the relationship from name2 to name 1 as well
         """
-        pass
+        if name1 not in self.connections:
+            self.connections[name1] ={}
+        if name2 not in self.connections:
+            self.connections[name2]= {}
+
+        self.connections[name1][name2]= relation
+
+        if reciprocal:
+            self.connections[name2][name1] = relation
 
     def forget(self, name1, name2):
         """Remove the connection between two people."""
-        pass
+        if name1 in self.connections and name2 in self.connections[name1]:
+            self.connections[name1].pop(name2)
+        if name2 in self.connections and name1 in self.connections[name2]:
+            self.connections[name2].pop(name1)
 
     def average_age(self):
         """Compute the average age of the group's members."""
@@ -55,8 +70,18 @@ if __name__ == "__main__":
     my_group = Group()
     # ...then add the group members one by one...
     my_group.add_person("Jill", 26, "biologist")
+    my_group.add_person("Zalika", 28, "artist")
+    my_group.add_person("John", 27, "writer")
+    my_group.add_person("Nash", 34, "chef")
+
     # ...then their connections
     my_group.connect("Jill", "Zalika", "friend")
+    my_group.connect("Jill", "John", "partner")
+    my_group.connect("Nash", "John", "cousin")
+    my_group.connect("Nash", "Zalika", "landlord")
+
+
+
     # ... then forget Nash and John's connection
     my_group.forget("Nash", "John")
 
